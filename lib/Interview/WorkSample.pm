e4478c2ba26e6671e10faeb7460ddf39e93125c0
Robert Novak <me@robert-novak.com>
2016-09-15 22:43:54 -0700

    some code

--- lib/Interview/WorkSample.pm
+++ lib/Interview/WorkSample.pm
@@ -8,10 +8,44 @@ use warnings;
 use Exporter;
 
 our $VERSION = "1.0.0";
-our @EXPORT_OK = qw(Solution);
+our @EXPORT_OK = qw(new Parse Solution);
 
-sub Solution() {
-
+sub new() {
+    return bless {}, 'Interview::WorkSample';
 }
+
+sub Parse($$) {
+    my ($package, $input) = @_;
+
+    my ($args, @lines) = ({}, split("\n", $input));
+
+    $args->{num_test_cases} = shift @lines;
+    $args->{test_case} = [];
+    
+    while (@lines) {
+        my $test_case = {};
+
+        ($test_case->{N}, $test_case->{K}) = split(" ", shift(@lines));
+
+        $test_case->{data} = split(" ", shift(@lines));
+
+        push($args->{test_case}, $test_case);
+    }
+
+    return $args;
+}
+
+sub Solution($;$) {
+    my ($package, $input) = @_;
+
+    $input = $package if ref $package eq 'Interview::WorkSample';
+
+    my $output = "";
+    
+
+
+    return $output;
+}
+
 
 1;
