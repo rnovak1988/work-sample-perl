5748093a5a836ad5e0b7809628210f17a5cb62ed
Robert Novak <me@robert-novak.com>
2016-09-15 22:20:11 -0700

    initial commit

---
+++ lib/Interview/WorkSample.pm
@@ -0,0 +1,16 @@
+#!perl
+
+package Interview::WorkSample;
+
+use strict;
+use warnings;
+
+use Exporter;
+
+our @EXPORT_OK = qw(Solution);
+
+sub Solution() {
+
+}
+
+1;
---
+++ t/001-use.t
@@ -0,0 +1,11 @@
+#!perl
+
+use Test::More;
+
+my $num_test_cases = 0;
+
+require_ok('Interview::WorkSample');
+$num_test_cases += 1;
+
+done_testing($num_test_cases);
+
